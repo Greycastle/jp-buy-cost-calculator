30b9b2caedb05264d66511f6d5ae27b8b8cb8e5b
David <ddikman@gmail.com>
2020-03-29 11:49:12 +0900

    Added class for handling brackets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,34 +23,56 @@ class FixedCost extends Cost {
   get cost(): number { return this._cost; }
 }
 
+class BracketRange {
+  breakPoint: number;
+  value: number;
+
+  constructor(breakPoint: number, value: number) {
+    this.breakPoint = breakPoint;
+    this.value = value;
+  }
+}
+
+class Brackets {
+  breakPoints: Array<BracketRange>;
+
+  constructor() {
+    this.breakPoints = new Array<BracketRange>();
+  }
+
+  add(breakPoint: number, value: number): void {
+    this.breakPoints.push(new BracketRange(breakPoint, value));
+  }
+
+  getValueFor(value: number) {
+    for (const breakPoint of this.breakPoints) {
+      if (value <= breakPoint.breakPoint) {
+        return breakPoint.value;
+      }
+    }
+    return this.breakPoints[this.breakPoints.length - 1].value;
+  }
+}
+
 class StampDuty extends Cost {
   private purchaseCost: number;
+  private brackets = new Brackets();
   constructor(purchaseCost: number) {
     super('Stamp', 'stuff');
     this.purchaseCost = purchaseCost;
+
+    this.brackets.add(1, 500);
+    this.brackets.add(5, 1000);
+    this.brackets.add(10, 5000);
+    this.brackets.add(50, 10000);
+    this.brackets.add(100, 30000);
+    this.brackets.add(500, 60000);
+    this.brackets.add(501, 160000);
   }
 
   private calculate(): number {
     const millions = this.purchaseCost / 1000000;
-    if (millions < 1) {
-      return 500;
-    }
-    if (millions < 5) {
-      return 1000;
-    }
-    if (millions < 10) {
-      return 5000;
-    }
-    if (millions < 50) {
-      return 10000;
-    }
-    if (millions < 100) {
-      return 30000;
-    }
-    if (millions < 500) {
-      return 60000;
-    }
-    return 160000;
+    return this.brackets.getValueFor(millions);
   }
 
   get cost(): number { return this.calculate(); }
